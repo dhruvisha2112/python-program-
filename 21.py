d42a01d1f035e858c93d54a4c1011c2d0ed06672
dhruvisha2112 <dhruvishathaker0@gmail.com>
2026-07-30 20:39:35 +0530

    Delete 21.py

--- 21.py
+++
@@ -1,8 +0,0 @@
-set1=set()
-set2={1,2,3,4}
-set3=set([5,6,7])
-set4={1,"python",3.14}
-print("empty set:",set1)
-print("set with values:",set2)
-print("set from list:",set3)
-print("mixed set:",set4)
