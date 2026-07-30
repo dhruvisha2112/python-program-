fcd3d44d1b741e3fbb82b8a7f087d716377c4c10
dhruvisha2112 <dhruvishathaker0@gmail.com>
2026-07-30 20:39:50 +0530

    Delete 23.py

--- 23.py
+++
@@ -1,8 +0,0 @@
-dict1={}
-dict2={'name':'alice','age':25}
-dict3=dict([('x',1),('y',2)])
-dict4={1:'one','two':2}
-print("empty dictionary:",dict1)
-print("with values:",dict2)
-print("using dict():",dict3)
-print("mixed keys:",dict4)
